a209c85258e63c04e769995d8be8fd92e9d6024a
brucehaha <henninglee2013@gmail.com>
2021-06-24 09:49:19 +1000

    web

--- learngo/errorhandling/filelistingserver/filelisting/handler.go
+++ learngo/errorhandling/filelistingserver/filelisting/handler.go
@@ -1,14 +1,33 @@
 package filelisting
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"reflect"
+	"strings"
 )
 
+const prefix = "/list/"
+
+type userError string
+
+func (e userError) Error() string {
+	return e.Message()
+}
+func (e userError) Message() string {
+	return string(e)
+}
 func HandleFileList(writer http.ResponseWriter, request *http.Request) error {
-	path := request.URL.Path[len("/list/"):]
+	path := request.URL.Path
+	if strings.Index(path, prefix) != 0 {
+		return userError("paths must start with " + prefix)
+	}
+	fmt.Println(len(path), reflect.TypeOf(path), path)
+
+	path = path[len(prefix):]
 
 	log.Println(path)
 	file, err := os.Open(path)
--- learngo/errorhandling/filelistingserver/web.go
+++ learngo/errorhandling/filelistingserver/web.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"os"
 
@@ -11,16 +12,30 @@ type appHandler func(writer http.ResponseWriter, request *http.Request) error
 
 func errorWrapper(handler appHandler) func(writer http.ResponseWriter, request *http.Request) {
 	return func(writer http.ResponseWriter, request *http.Request) {
+		defer func() {
+			if r := recover(); r != nil {
+				log.Printf("Panic: %v", r)
+				http.Error(writer,
+					http.StatusText(http.StatusInternalServerError),
+					http.StatusInternalServerError)
+			}
+
+		}()
 		err := handler(writer, request)
 		if err != nil {
+			log.Printf("Error occured "+"handling request: %s", err.Error())
+			if userError, ok := err.(userError); ok {
+				http.Error(writer, userError.Message(), http.StatusBadRequest)
+				return
+			}
 			code := http.StatusOK
 			switch {
 			case os.IsNotExist(err):
 				code = http.StatusNotFound
-			case os.IsNotExist(err):
+			case os.IsPermission(err):
 				code = http.StatusForbidden
-				//default:
-				//	code = http.StatusInternalServerError
+			default:
+				code = http.StatusInternalServerError
 			}
 			http.Error(writer,
 				http.StatusText(code),
@@ -30,12 +45,18 @@ func errorWrapper(handler appHandler) func(writer http.ResponseWriter, request *
 	}
 }
 
+type userError interface {
+	error
+	Message() string
+}
+
 func main() {
 
-	http.HandleFunc("/list/", errorWrapper(filelisting.HandleFileList))
+	http.HandleFunc("/", errorWrapper(filelisting.HandleFileList))
 
 	err := http.ListenAndServe(":8888", nil)
 	if err != nil {
 		panic(err)
 	}
+
 }
